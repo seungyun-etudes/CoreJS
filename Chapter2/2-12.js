4bbaf9ad152b22c958198c0e230826fb6ea8b6a5
SeungYunBaek <ryusim@cau.ac.kr>
2022-08-31 19:48:44 +0900

    [chapter2] Example 12

---
+++ Chapter2/2-12.js
@@ -0,0 +1,14 @@
+// console.log(sum(3, 4));
+
+var sum = function(x, y) {
+    return x + y;
+};
+
+var a = sum(1, 2);
+
+var sum = function(x, y) {
+    return x + ' + ' + y + ' = ' + (x + y);
+};
+
+var c = sum(1, 2);
+console.log(a, c);
